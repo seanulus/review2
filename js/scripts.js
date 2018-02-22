751e65253e8e07afef7e48f42a03a0103186b6cc
Guest User <Guest@Epicodus-9D.local>
2018-02-21 20:11:44 -0800

    Added CSS to hide ID's and set up JS with if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-  $("selection").submit(function(event) {
+  $("#selection").submit(function(event) {
+    //debugger
+    var name = parseInt($("#name").val());
+    var quest = parseInt($("#quest").val());
+    var color = parseInt($("#color").val());
+    var location = parseInt($("#location").val());
+    var axe = parseInt($("#axe").val());
+    var total = name + quest + color + location + axe
+
+    if (total >= 4) {
+      $("#fantasy").fadeIn();
+      $("#power, #black").hide();
+    } else if (total >= 9) {
+      $("#power").slideIn();
+      $("#fantasy, #black").hide();
+    } else (total > 13) {
+      $("#black").fadeIn();
+      $("#fantasy, #power")
+    }
+
     event.preventDefault()
   });
 });
